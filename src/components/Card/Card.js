b8f8482759144ad6b37ae38a5fa9518f099c9896
nicolas.martin <nicolas.martin@valtech.fr>
2019-07-06 11:24:59 +0100

    add class component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Card.scss'
 
-const Card = (props) => {
+/* const Card = (props) => {
 
     return (
         <div className="card">
@@ -11,6 +11,19 @@ const Card = (props) => {
             <p style={{display: !props.item.year && "none"}}>{props.item.year}</p>
         </div>
     )
+} */
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <img src={this.props.item.image} alt="" />
+                <h3>{this.props.item.title}</h3>
+                <p>{this.props.item.brand} {this.props.item.model} {this.props.item.version}</p>
+                <p style={{display: !this.props.item.year && "none"}}>{this.props.item.year}</p>
+            </div>
+       )
+    }
 }
 
 export default Card
