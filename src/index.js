e10dd9be159870ee4411f49da10a3e22faf5e3a7
nicolas.martin <nicolas.martin@valtech.fr>
2019-07-12 22:38:06 +0100

    add redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* import store */
+import index from "./js/index"
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 /* // If you want your app to work offline and load faster, you can change
