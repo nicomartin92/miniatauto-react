790ac5e83a4933dc690028d09252917d309a7f73
Nicolas Martin <nicolas27martin@gmail.com>
2019-09-06 14:56:38 +0200

    add new tests

--- src/components/PanelNav/__tests__/PanelNav.test.js
+++ src/components/PanelNav/__tests__/PanelNav.test.js
@@ -3,21 +3,17 @@ import { shallow, render, mount } from 'enzyme';
 import PanelNav from '../PanelNav';
 
 describe('<PanelNav>', () => {
+    const wrapper = shallow(<PanelNav />);
+
     it('should do panelSwitcher to be false on button close', () => {
-        const wrapper = shallow(<PanelNav />);
         const firstButton = wrapper.find('.buttonClose');
-
         firstButton.simulate('click');
-
         expect(wrapper.state().isOpen).toEqual(false);
     });
 
     it('should do panelSwitcher to be false on overlay expanded', () => {
-        const wrapper = shallow(<PanelNav />);
         const firstButton = wrapper.find('.overlay');
-
         firstButton.simulate('click');
-
         expect(wrapper.state().isOpen).toEqual(false);
     });
 });
